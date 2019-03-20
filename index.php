f5ba58a1f575ccd527203af58e1fe115510cdb70
timos <timo.slond@student.rocvf.nl>
2019-03-20 09:35:11 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: timos
+ * Date: 24-1-2019
+ * Time: 11:37
+ */
+$a = "Ik heb niet gefaald.";
+$b = "die niet werken. ";
+$c = " 10000 manieren gevonden ";
+$d = " Ik heb alleen";
+
+echo $a.$d.$c.$b;
+
+$a2 = "die geen fouten";
+$b2 = "en";
+$c2 =  "niet.";
+
+echo "Mensen ".$a2.$b2.", werken ".$c2;
+
+
+$a3 = "ry";
+$b3 = ", but";
+$c3 = "fail";
+
+echo "T".$a3." and ".$b3.$c3." never try.";
+
+$a4 = "\"omgaan ";
+$b4 = "met ";
+$c4 = "teleu";
+
+echo " De cursus".$a4.$b4.$c4."rstelling\" kan vanavond helaas niet doorgaan.";
